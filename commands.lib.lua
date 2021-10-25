b2fad2a4cc30b0c8ae4e59135abb706a3c523a08
Sebastian Huebner <sebastian@hueb-ner.de>
2021-10-25 13:18:16 +0200

    the meetings command is not an alias anymore and now lists all meetings plus it's participants so its basically a combination of the list_meetings and participants command

--- commands.lib.lua
+++ commands.lib.lua
@@ -11,9 +11,8 @@ local commands = {}
 function commands.help()
   return [[The following commands are available:
 * **list_meetings**         - lists all open meetings and the number of participants
-* **meetings**              - alias for **list_meetings**
-* **participants $meeting** - lists all participants of a meeting - replace $meeting with the meeting name
-* **teilnehmer**            - alias **participants**]]
+* **meetings**              - lists all open meetings and their participants
+* **participants $meeting** - lists all participants of a meeting - replace $meeting with the meeting name]]
 end
 
 --- Returns open meetings rooms with user number
@@ -58,10 +57,33 @@ function commands.participants(room_name)
   return table.concat(parti_list, '\n')
 end
 
+-- Returns open meeting rooms and their participants
+local meetings_parti_line_format = '   * %s'
+function commands.meetings()
+  local room_list = { 'The following meetings are currently open:\n' }
 
---- Aliases
-commands.meetings = commands.list_meetings
-commands.teilnehmer = commands.participants
+  for room in ut.each_room() do
+    local parti_list = {}
+    local parti_count = 0
+
+    for occupant in ut.each_occupant(room) do
+      local nick = ut.get_jitsi_nick_from_occupant(occupant)
+      parti_count = parti_count + 1
+
+      table.insert(parti_list, meetings_parti_line_format:format(nick and nick:find('%S') and nick or 'Fellow Jitster'))
+    end
+
+    local room_name = tostring(room:get_name())
+    table.insert(room_list, meetings_line_format:format(room_name, room_name, parti_count))
+    table.insert(room_list, table.concat(parti_list, '\n'))
+  end
+
+  if #room_list == 1 then
+    return 'Currently there are no open meetings.'
+  end
+
+  return table.concat(room_list, '\n')
+end
 
 
 return commands
